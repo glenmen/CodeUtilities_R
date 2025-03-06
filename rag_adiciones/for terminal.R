d692993fe45344199df75ebdb53ff6f5c60095b7
robartgan <robert.arteaga.gan@gmail.com>
2025-03-06 13:18:17 -0500

    adicion de nueva linea para terminal

--- rag_adiciones/for terminal.R
+++ rag_adiciones/for terminal.R
@@ -1,3 +1,5 @@
 git config --global user.name "Your Name"
 git config --global user.email "your.email@example.com"
 git config --global --list  # Check settings
+git commit -m "Fixed commit"
+gpg --list-secret-keys --keyid-format LONG
